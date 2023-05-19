5ad61ba8f2198a4be381e50bb5abb533d7709e7e
Chukwubuikem Chukwudi <myecodevelopment@gmail.com>
2023-05-20 00:20:32 +0100

    Initial Commit

--- ARQuickLook/ViewController.swift
+++ ARQuickLook/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import QuickLook
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var panelImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(presentQuicklook))
+        panelImage.addGestureRecognizer(tapGesture)
     }
 
+    @objc func presentQuicklook() {
+        let previewViewController = QLPreviewController()
+        previewViewController.dataSource = self
+        previewViewController.delegate = self
+        present(previewViewController, animated: true, completion: nil)
+    }
 
+}
+
+extension ViewController: QLPreviewControllerDelegate {
+    func previewController(_ controller: QLPreviewController, transitionViewFor item: QLPreviewItem) -> UIView? {
+        return panelImage
+    }
+}
+
+extension ViewController: QLPreviewControllerDataSource {
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return 1
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        let fileUrl = Bundle.main.url(forResource: "solar_panels", withExtension: "usdz")!
+        return fileUrl as QLPreviewItem
+    }
 }
 
